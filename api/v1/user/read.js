0f147f26b97b0d5b7468fbf5e135e0cf45085138
vova <vladimir.gubarenko@gmail.com>
2023-06-14 15:37:09 +0300

    add json schema validation

--- api/v1/user/read.js
+++
@@ -1,7 +0,0 @@
-({
-  access: 'public',
-  method: async ({ id }) => {
-    const data = (await db.crud('User').read(id))?.rows || [];
-    return { status: 'ok', data };
-  },
-});
--- config/database.js
+++
@@ -1,8 +0,0 @@
-({
-  host: '127.0.0.1',
-  port: 5432,
-  database: 'quiz',
-  user: 'postgres',
-  schema: 'public',
-  password: 'postgres',
-});
